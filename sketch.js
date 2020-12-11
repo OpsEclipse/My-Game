24d587996c19c899f7169422c57199400860a0c7
OpsEclipse <70162771+OpsEclipse@users.noreply.github.com>
2020-12-10 19:38:35 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var ground,groundS,bucketL,bucketR,bucketB;
 
 var bucketLS,bucketRS,bucketBS;
 
-var cube1,cubeG;
+var coin1,cubeG;
 
 var PLAY = 1;
 var END = 0;
@@ -20,11 +20,15 @@ var score = 0;
 var back, coin;
 var restart, restartImg;
 var bucketImg;
+var scoreMp3, overMp3;
+var coin1,coin2,coin3,coin4
 function preload(){
   back = loadImage("my game back.jpg");
   coin = loadImage("dollar.png");
   restartImg = loadImage("restart.png");
   bucketImg = loadImage("bucket.png")
+  overMp3 = loadSound("game-over.mp3");
+  scoreMp3 = loadSound("score.mp3");
 }
 function setup() {
 
@@ -42,29 +46,29 @@ function setup() {
   bucketBS.addImage(bucketImg);
   bucketBS.scale = 0.4;
 
-  cube1 = createSprite(900,-50,50,50);
-  cube1.velocityY = 10;
-  cube1.addImage(coin);
-  cube1.scale = 0.2;
+  coin1 = createSprite(900,-50,50,50);
+  coin1.velocityY = 10;
+  coin1.addImage(coin);
+  coin1.scale = 0.2;
 
-  cube2 = createSprite(300,-250,50,50);
-  cube2.velocityY = 10;
-  cube2.addImage(coin)
-  cube2.scale = 0.2;
+  coin2 = createSprite(300,-250,50,50);
+  coin2.velocityY = 10;
+  coin2.addImage(coin)
+  coin2.scale = 0.2;
 
-  cube3 = createSprite(1100,-400,50,50);
-  cube3.velocityY = 10;
-  cube3.addImage(coin);
-  cube3.scale = 0.2;
+  coin3 = createSprite(1100,-400,50,50);
+  coin3.velocityY = 10;
+  coin3.addImage(coin);
+  coin3.scale = 0.2;
 
-  cube4 = createSprite(1400,-650,50,50);
-  cube4.velocityY = 10;
-  cube4.addImage(coin)
-  cube4.scale = 0.2;
+  coin4 = createSprite(1400,-650,50,50);
+  coin4.velocityY = 10;
+  coin4.addImage(coin)
+  coin4.scale = 0.2;
 
-restart = createSprite(50,50,50,50);
-restart.addImage(restartImg);
-restart.scale = 0.15;
+  restart = createSprite(50,50,50,50);
+  restart.addImage(restartImg);
+  restart.scale = 0.15;
 
   groundS.visible = false;
   
@@ -80,35 +84,48 @@ function draw() {
 
   if(gameState === PLAY){
 
+    
+
     MoveB();
 
-    cube1.velocityY = 10;
-    cube2.velocityY = 10;
-    cube3.velocityY = 10;
-    cube4.velocityY = 10;
+    coin1.velocityY = 10;
+    coin2.velocityY = 10;
+    coin3.velocityY = 10;
+    coin4.velocityY = 10;
 
-    if(cube1.isTouching(bucketBS)){
+    if(coin1.isTouching(bucketBS)){
       score++;
-      cube1.y = -50;
-      cube1.x = Math.round(random(50,1550));
+      coin1.y = -50;
+      coin1.x = Math.round(random(50,1550));
+      scoreMp3.play();
     }
 
-    else if(cube2.isTouching(bucketBS)){
+    else if(coin2.isTouching(bucketBS)){
       score++;
-      cube2.y = -50;
-      cube2.x = Math.round(random(50,1550));
+      coin2.y = -50;
+      coin2.x = Math.round(random(50,1550));
+      scoreMp3.play();
     }
-    else if(cube3.isTouching(bucketBS)){
+    else if(coin3.isTouching(bucketBS)){
       score++;
-      cube3.y = -50;
-      cube3.x = Math.round(random(50,1550));
+      coin3.y = -50;
+      coin3.x = Math.round(random(50,1550));
+      scoreMp3.play();
     }
-    else if(cube4.isTouching(bucketBS)){
+    else if(coin4.isTouching(bucketBS)){
       score++;
-      cube4.y = -50;
-      cube4.x = Math.round(random(50,1550));
+      coin4.y = -50;
+      coin4.x = Math.round(random(50,1550));
+      scoreMp3.play();
     }
-    else if(cube1.isTouching(groundS)||cube2.isTouching(groundS)||cube3.isTouching(groundS)||cube4.isTouching(groundS)){
+    if(score%10 ===0){
+      coin1.velocityY = coin1.velocityY+2;
+      coin2.velocityY = coin2.velocityY+2;
+      coin3.velocityY = coin3.velocityY+2;
+      coin4.velocityY = coin4.velocityY+2;
+    }
+    else if(coin1.isTouching(groundS)||coin2.isTouching(groundS)||coin3.isTouching(groundS)||coin4.isTouching(groundS)){
+      overMp3.play();
       gameState = END;
     }
 
@@ -118,15 +135,16 @@ function draw() {
     fill("#15f4ee");
     textAlign(CENTER,CENTER);
     text("Game Over",900,250);
-    cube1.velocityY = 0;
-    cube2.velocityY = 0;
-    cube3.velocityY = 0;
-    cube4.velocityY = 0;
+    coin1.velocityY = 0;
+    coin2.velocityY = 0;
+    coin3.velocityY = 0;
+    coin4.velocityY = 0;
     if(mousePressedOver(restart)){
      reset();
     }
   }
 bucketBS.x = bucketB.x;
+
 
 
   drawSprites();
@@ -141,15 +159,15 @@ function MoveB(){
   }
 }
 function reset(){
-  cube1.y = -50;
-  cube2.y = -250;
-  cube3.y = -400;
-  cube4.y = -650;
+  coin1.y = -50;
+  coin2.y = -250;
+  coin3.y = -400;
+  coin4.y = -650;
 
-  cube1.x = Math.round(random(50,1550));
-  cube2.x = Math.round(random(50,1550));
-  cube3.x = Math.round(random(50,1550));
-  cube4.x = Math.round(random(50,1550));
+  coin1.x = Math.round(random(50,1550));
+  coin2.x = Math.round(random(50,1550));
+  coin3.x = Math.round(random(50,1550));
+  coin4.x = Math.round(random(50,1550));
   score = 0;
   gameState = PLAY;
 }
